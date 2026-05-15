6f323c9b2d321584e046289a1ab79beb8a446034
jnshiyan <shi.2046@163.com>
2026-05-15 15:43:56 +0800

    refactor(repo): remove legacy admin and generator modules

--- admin/src/main/java/com/quantroad/admin/repository/StockBasicRepository.java
+++
@@ -1,8 +0,0 @@
-package com.quantroad.admin.repository;
-
-import com.quantroad.admin.entity.StockBasicEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StockBasicRepository extends JpaRepository<StockBasicEntity, Long> {
-}
-
